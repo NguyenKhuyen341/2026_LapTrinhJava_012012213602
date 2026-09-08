ae398e2dff920bab529bf2ac30b4738496de2706
NguyenKhuyen341 <khuyen2132005@gmail.com>
2026-09-09 00:10:07 +0700

    Fix lỗi kết nối database và nâng cấp phiên bản LomBok

--- src/main/java/com/planbookai/backend/domain/model/studentdata/HocSinhRepository.java
+++ src/main/java/com/planbookai/backend/domain/model/studentdata/HocSinhRepository.java
@@ -8,11 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface HocSinhRepository extends JpaRepository<HocSinh, Long> {
-
     List<HocSinh> findByGiaoVienId(Long giaoVienId);
-
     Optional<HocSinh> findByIdAndGiaoVienId(Long id, Long giaoVienId);
-
     boolean existsByMaSoHocSinhAndGiaoVienId(
             String maSoHocSinh,
             Long giaoVienId
